c11f6fec308535e5a3f024ad095f6e0a88c12110
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-06-29 18:27:22 +0200

    add Task 04

---
+++ Task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿// "Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел"
+
+Console.WriteLine("Введите 1-е число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3-е число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = num1;
+
+if (maxNum < num2)
+{
+    maxNum = num2;
+    if (maxNum < num3)
+    {
+        maxNum = num3;
+    }
+}
+Console.WriteLine($"Максимальное число: {maxNum}");
