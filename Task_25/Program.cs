6134ddb0f76f8942ef089716a33d4625b62be7f0
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-07-06 17:30:42 +0200

    add Task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿// "Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B"
+
+int numA = new Random().Next(1, 9);
+int numB = new Random().Next(1, 9);
+
+int res = numA;
+
+for (int count = 1; count < numB; count++)
+{
+    res *= numA;
+}
+Console.WriteLine($"{res} = {numA} в степени {numB}");
