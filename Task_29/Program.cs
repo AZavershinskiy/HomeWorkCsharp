3e17cc832c39713735c3f3d43936040508c8b82d
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-07-07 14:37:41 +0200

    add Task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,11 @@
+﻿// "Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран"
+
+int N = new Random().Next(2, 50);
+int[] nums = new int[N];
+
+for (int indx = 0; indx < nums.Length; indx++)
+{
+    nums[indx] = new Random().Next(1, 99);
+    Console.Write(nums[indx] + ", ");
+}
+Console.WriteLine($" - массив из {N} элементов"); // <= для эстетики в консоли
