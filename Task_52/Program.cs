3a5b74df6ceae66ec5d4a38590981c9df04d2812
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-07-20 14:12:59 +0200

    add Task 52

---
+++ Task_52/Program.cs
@@ -0,0 +1,46 @@
+﻿// "Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце"
+
+int m = InputInt("Введите колличество строк: ");
+int n = InputInt("Введите колличество столбцов: ");
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    double average = sum / m;
+    Console.Write(Math.Round(average, 2) + "; "); // округлил, а то глаз дергается  =D
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
