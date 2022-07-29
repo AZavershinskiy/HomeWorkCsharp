3213db986b6927328c39b6de854736dd8ffcb4fc
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-07-29 12:10:30 +0200

    add Task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,31 @@
+﻿// "Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N"
+
+int m = InputInt("Введите число M: ");
+int n = InputInt("Введите число N: ");
+
+Console.WriteLine(NaturalNumbers(m, n));
+
+int NaturalNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        if (n == m)
+            return m;
+        else
+            Console.Write(NaturalNumbers(m, n - 1) + ", ");
+        return n;
+    }
+    else
+    {
+        if (n == m)
+            return m;
+        else
+            Console.Write(NaturalNumbers(m, n + 1) + ", ");
+        return n;
+    }
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
